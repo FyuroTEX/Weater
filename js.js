f1c98493e6f91422343f87af78569d9783335aa1
FyuroTEX <yurilev4@gmail.com>
2018-04-26 15:05:24 +0300

    weater + geolocation

--- js.js
+++ js.js
@@ -1,52 +1,35 @@
-/*
-function getWeather(callback) {
-    $.ajax({
-      dataType: "jsonp",
-      url: 'https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139',
-      success: callback
-    });
-}
+var api = "https://fcc-weather-api.glitch.me/api/current?";
+var lat, lon;
+var weather;
+var temperature;
+var cloudsimg;
 
-// get data:
-getWeather(function (data) {
-    console.log('weather data received');
-    console.log(data.weather[0].description);
-    console.log(data.weather[0].main);
-});
-*/
 $(document).ready(function() {
 
-      var weather;
-        var temperature;
-      function getTemp(callback) {
-
-        $.ajax({
-          dataType: 'jsonp',
-          url: 'https://fcc-weather-api.glitch.me/api/current?lat=50&lon=30',
-
-          success: callback
-        });
-}
-        getTemp(function(data) {
-weather = data.weather[0].description;
-temperature = data.main.temp;
-$('#temp').text(data.main.temp + "°C");
-$('.city').text(data.name);
-$('#sky').html(weather + '<img src="'+ data.weather[0].icon + '">')
-          console.log('weather data received');
-          console.log(data.weather[0].description);
-          console.log(data.weather[0].main);
-
-        });
-
-
-    $('#getQuote').on('click', function(event) {
-      event.preventDefault;
-
-getTemp();
-
-    alert(weather);
-    alert(temperature);
-
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = "lat=" + position.coords.latitude;
+      var lon = "lon=" + position.coords.longitude;
+      getWeather(lat, lon);
     });
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+
+  function getWeather(lat, lon) {
+    var urlString = api + lat + "&" + lon;
+    $.ajax({
+      url: urlString,
+      success: function(data) {
+
+        weather = data.weather[0].description;
+        temperature = data.main.temp;
+        cloudsimg = data.weather[0].icon;
+        $('#temp').text(data.main.temp + "°C");
+        $('.city').text(data.name);
+        cloudsimg ? $('#sky').html(weather + '<img src="' + cloudsimg + '">') :
+          $('#sky').html(weather + ' ' + '<i class="fas fa-cloud"></i>');
+      }
     });
+  }
+});
